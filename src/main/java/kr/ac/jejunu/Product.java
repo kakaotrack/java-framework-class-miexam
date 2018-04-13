ef7600107d7b1dc6d599536bca92736e27efa5d5
hulk.h <hulk.h@kakaocorp.com>
2018-04-13 18:27:38 +0900

    init

---
+++ src/main/java/kr/ac/jejunu/Product.java
@@ -0,0 +1,31 @@
+package kr.ac.jejunu;
+
+public class Product {
+    private Long id;
+    private String title;
+    private Integer price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+}
