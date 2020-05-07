8de739fcee1037aa31e8d86cbf5253f0614bc608
hulk.h <hulk.h@kakaocorp.com>
2020-05-07 17:41:36 +0900

    2020 실습과제

--- src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
+++ src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
@@ -1,9 +1,8 @@
 package kr.ac.jejunu.userdao;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -11,8 +10,8 @@ import static org.hamcrest.core.Is.is;
 public class UserDaoTests {
     @Test
     public void testGet() throws SQLException, ClassNotFoundException {
-        Long id = 1l;
-        String name = "허윤호";
+        Integer id = 1;
+        String name = "hulk";
         String password = "1234";
         UserDao userDao = new UserDao();
         User user = userDao.get(id);
