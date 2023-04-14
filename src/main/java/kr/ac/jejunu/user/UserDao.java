58976bc226793669bd9e0f08db37cc75c534a34a
hulk.h <hulk.h@kakaocorp.com>
2023-04-14 13:27:45 +0900

    2023 중간고사

---
+++ src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -0,0 +1,23 @@
+package kr.ac.jejunu.user;
+
+import java.sql.*;
+
+public class UserDao {
+    public User findById(Long id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jeju", "jeju", "jejupw");
+        PreparedStatement preparedStatement = connection.prepareStatement("select id, name, password from userinfo where ?");
+        preparedStatement.setLong(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        User user = new User();
+        user.setId(resultSet.getLong("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return user;
+    }
+}
