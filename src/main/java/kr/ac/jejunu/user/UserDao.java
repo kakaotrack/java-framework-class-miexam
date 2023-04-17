45f91e25fc7b0764fc41e43ac5b8cdcb73e71936
hulk.h <hulk.h@kakaocorp.com>
2023-04-17 09:14:09 +0900

    query 오류 수정

--- src/main/java/kr/ac/jejunu/user/UserDao.java
+++ src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -6,7 +6,7 @@ public class UserDao {
     public User findById(Long id) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jeju", "jeju", "jejupw");
-        PreparedStatement preparedStatement = connection.prepareStatement("select id, name, password from userinfo where ?");
+        PreparedStatement preparedStatement = connection.prepareStatement("select id, name, password from userinfo where id = ?");
         preparedStatement.setLong(1, id);
         ResultSet resultSet = preparedStatement.executeQuery();
         resultSet.next();
